a0403e213179dd31c82dbc14e62017a5eb029978
Gustavo Fontana <gustavoasf77@gmail.com>
2026-03-02 22:59:07 -0300

    implements jwt validator and api config

---
+++ src/main/java/com/gustass/ecommerce_java/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.gustass.ecommerce_java.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "3213wfwefreferf324534egfdg7645645";
+    public static final String JWT_HEADER = "Authorization";
+}
